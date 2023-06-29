a8e56a81cd94c22a9a05539f4edd25088a3f805c
Daniel Betancur Giraldo <betangil238@hotmail.com>
2023-06-29 14:07:07 -0500

    Solucion ejercicio extra 1

---
+++ src/ExtraJercicio1/MainCancion.java
@@ -0,0 +1,17 @@
+package ExtraJercicio1;
+
+public class MainCancion {
+//	1. Desarrollar una clase Cancion con los siguientes atributos: titulo y autor. Se deberá́
+//	definir además dos constructores: uno vacío que inicializa el titulo y el autor a cadenas
+//	vacías y otro que reciba como parámetros el titulo y el autor de la canción. Se deberán
+//	además definir los métodos getters y setters correspondientes.
+
+	public static void main(String[] args) {
+		Cancion cancion = new Cancion();
+		Cancion cancion1 = new Cancion("Rolling in the deep","Adele");
+		System.out.println(cancion.toString());
+		System.out.println(cancion1.toString());
+
+	}
+
+}
